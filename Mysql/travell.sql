cc6ec2df08980a1739f62aee91b4282411e27317
Duy Anh <danhy989@gmail.com>
2019-03-21 12:08:53 +0700

    Update travell.sql

--- Mysql/travell.sql
+++ Mysql/travell.sql
@@ -1,77 +1,241 @@
-CREATE TABLE ROLE
-(
-	ID_ROLE INT(10) NOT NULL AUTO_INCREMENT,
-    ROLE VARCHAR(45) NOT NULL,
-    PRIMARY KEY (ID_ROLE)
-);
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: uittravel
+-- ------------------------------------------------------
+-- Server version	8.0.15
 
-CREATE TABLE USER
-(
-	ID_USER INT(10) NOT NULL AUTO_INCREMENT,
-    USERNAME VARCHAR(45) NOT NULL,
-    PASSWORD VARCHAR(45) NOT NULL,
-    NAME VARCHAR(45) NOT NULL,
-    EMAIL VARCHAR(45) DEFAULT NULL,
-    PHONE INT(10) DEFAULT NULL,
-    STATUS INT(3) NOT NULL DEFAULT '0',
-    PRIMARY KEY(ID_USER)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE USER_ROLE
-(
-	ID_USER INT(10) NOT NULL,
-    ID_ROLE INT(10) NOT NULL,
-    PRIMARY KEY(ID_USER, ID_ROLE), 
-	CONSTRAINT `FK_ROLE` FOREIGN KEY (ID_ROLE) REFERENCES ROLE(ID_ROLE),
-    CONSTRAINT `FK_USER` FOREIGN KEY (ID_USER) REFERENCES USER(ID_USER)
-);
+--
+-- Table structure for table `comment`
+--
 
-CREATE TABLE PROVINCE
-(
-	ID_PROVINCE VARCHAR(10) NOT NULL,
-    NAME VARCHAR(20) NOT NULL,
-    PRIMARY KEY(ID_PROVINCE)
-);
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comment` (
+  `ID_CMT` varchar(10) NOT NULL,
+  `NAME` varchar(30) NOT NULL,
+  `CONTENT` longtext,
+  `ID_TOUR` varchar(10) NOT NULL,
+  PRIMARY KEY (`ID_CMT`),
+  KEY `FK_COMMENT_TOURINFO` (`ID_TOUR`),
+  CONSTRAINT `FK_COMMENT_TOURINFO` FOREIGN KEY (`ID_TOUR`) REFERENCES `tour_information` (`ID_TOUR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE TOUR
-(
-	ID_TOUR VARCHAR(10) NOT NULL,
-    NAME VARCHAR(45) NOT NULL,
-    IMAGE VARCHAR(100) NOT NULL,
-    PRIMARY KEY(ID_TOUR)
-);
+--
+-- Dumping data for table `comment`
+--
 
-CREATE TABLE TOUR_INFORMATION
-(
-	ID_TOUR VARCHAR(10) NOT NULL,
-    DETAIL LONGTEXT,
-    IMAGE_FULL VARCHAR(100) NOT NULL,
-    PRICE DOUBLE NOT NULL DEFAULT '0',
-    ID_PROVINCE VARCHAR(10),
-    STATUS INT(2) NOT NULL DEFAULT '0',
-    PRIMARY KEY(ID_TOUR),
-    CONSTRAINT `FK_TOUR_TOURINFO` FOREIGN KEY (ID_TOUR) REFERENCES TOUR(ID_TOUR),
-	CONSTRAINT `FK_TOUR_PROVINCE` FOREIGN KEY (ID_PROVINCE) REFERENCES PROVINCE(ID_PROVINCE)
-);
-CREATE TABLE COMMENT 
-(
-	ID_CMT VARCHAR(10) NOT NULL,
-    NAME VARCHAR(30) NOT NULL,
-    CONTENT LONGTEXT,
-    ID_TOUR VARCHAR(10) NOT NULL,
-	PRIMARY KEY(ID_CMT),
-	CONSTRAINT `FK_COMMENT_TOURINFO` FOREIGN KEY (ID_TOUR) REFERENCES TOUR_INFORMATION(ID_TOUR)
-);
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE ORDER_TOUR
-(
-	ID_ORDER VARCHAR(10) NOT NULL,
-    ID_TOUR VARCHAR(10) NOT NULL,
-    NAME VARCHAR(30) NOT NULL,
-    PHONE INT(10) NOT NULL,
-    ADDRESS VARCHAR(100) NOT NULL,
-    PASSENGERS INT(2) NOT NULL,
-    DECRIPTION LONGTEXT,
-    PRIMARY KEY(ID_ORDER),
-    CONSTRAINT `FK_ORDER_TOURINFO` FOREIGN KEY (ID_TOUR) REFERENCES TOUR(ID_TOUR)
-)
+--
+-- Table structure for table `order_tour`
+--
+
+DROP TABLE IF EXISTS `order_tour`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `order_tour` (
+  `ID_ORDER` varchar(10) NOT NULL,
+  `ID_TOUR` varchar(10) NOT NULL,
+  `NAME` varchar(30) NOT NULL,
+  `PHONE` int(10) NOT NULL,
+  `ADDRESS` varchar(100) NOT NULL,
+  `PASSENGERS` int(2) NOT NULL,
+  `DECRIPTION` longtext,
+  PRIMARY KEY (`ID_ORDER`),
+  KEY `FK_ORDER_TOURINFO` (`ID_TOUR`),
+  CONSTRAINT `FK_ORDER_TOURINFO` FOREIGN KEY (`ID_TOUR`) REFERENCES `tour` (`ID_TOUR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_tour`
+--
+
+LOCK TABLES `order_tour` WRITE;
+/*!40000 ALTER TABLE `order_tour` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_tour` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `province`
+--
+
+DROP TABLE IF EXISTS `province`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `province` (
+  `ID_PROVINCE` varchar(10) NOT NULL,
+  `NAME` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID_PROVINCE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `province`
+--
+
+LOCK TABLES `province` WRITE;
+/*!40000 ALTER TABLE `province` DISABLE KEYS */;
+/*!40000 ALTER TABLE `province` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `role` (
+  `ID_ROLE` int(10) NOT NULL AUTO_INCREMENT,
+  `ROLE` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID_ROLE`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'admin'),(2,'manager'),(3,'employee'),(4,'customer');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tour`
+--
+
+DROP TABLE IF EXISTS `tour`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tour` (
+  `ID_TOUR` varchar(10) NOT NULL,
+  `NAME` varchar(45) NOT NULL,
+  `IMAGE` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID_TOUR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tour`
+--
+
+LOCK TABLES `tour` WRITE;
+/*!40000 ALTER TABLE `tour` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tour` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tour_information`
+--
+
+DROP TABLE IF EXISTS `tour_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tour_information` (
+  `ID_TOUR` varchar(10) NOT NULL,
+  `DETAIL` longtext,
+  `IMAGE_FULL` varchar(100) NOT NULL,
+  `PRICE` double NOT NULL DEFAULT '0',
+  `ID_PROVINCE` varchar(10) DEFAULT NULL,
+  `STATUS` int(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID_TOUR`),
+  KEY `FK_TOUR_PROVINCE` (`ID_PROVINCE`),
+  CONSTRAINT `FK_TOUR_PROVINCE` FOREIGN KEY (`ID_PROVINCE`) REFERENCES `province` (`ID_PROVINCE`),
+  CONSTRAINT `FK_TOUR_TOURINFO` FOREIGN KEY (`ID_TOUR`) REFERENCES `tour` (`ID_TOUR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tour_information`
+--
+
+LOCK TABLES `tour_information` WRITE;
+/*!40000 ALTER TABLE `tour_information` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tour_information` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `ID_USER` int(10) NOT NULL AUTO_INCREMENT,
+  `USERNAME` varchar(45) NOT NULL,
+  `PASSWORD` tinytext NOT NULL,
+  `NAME` varchar(45) NOT NULL,
+  `EMAIL` varchar(45) DEFAULT NULL,
+  `PHONE` int(10) DEFAULT NULL,
+  `STATUS` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID_USER`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','$2a$10$ptCLbxaJ7k7u1Kn185IpnOGYR8/3HL9pMKORoFEJXvDSfE1uBC81W','admin1','admin@gmail.com',12345678,1),(2,'manager','$2a$10$YUGJfbnxV.gdk16uD5d5Q./ZHb4mlGFllzZgodVt1VjczE6A6/6r6','manager1',NULL,NULL,1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_role` (
+  `ID_USER` int(10) NOT NULL,
+  `ID_ROLE` int(10) NOT NULL,
+  PRIMARY KEY (`ID_USER`,`ID_ROLE`),
+  KEY `FK_ROLE` (`ID_ROLE`),
+  CONSTRAINT `FK_ROLE` FOREIGN KEY (`ID_ROLE`) REFERENCES `role` (`ID_ROLE`),
+  CONSTRAINT `FK_USER` FOREIGN KEY (`ID_USER`) REFERENCES `user` (`ID_USER`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (1,1),(2,2);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-21 12:08:07
