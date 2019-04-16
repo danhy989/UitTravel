cdfbb2705f233bda07ef31d2b32710dce90f612d
NTD <nhokcute533@gmail.com>
2019-04-16 11:34:12 +0700

    fix

--- frontend/js/post.js
+++ frontend/js/post.js
@@ -2,9 +2,14 @@
 
 function summit(){
 	var post = {};
-	var formData =JSON.stringify($('#myForm').serializeArray());
-	var idTour = document.getElementById('id').innerHTML;
-	post.order = formData;
+	var formData ={};
+	var idTour =parseInt(document.getElementById('id').innerHTML);
+	formData.name = document.getElementById("myForm").elements[0].value;
+	formData.phone =parseInt(document.getElementById("myForm").elements[1].value);
+	formData.address = document.getElementById("myForm").elements[2].value;
+	formData.passengers = parseInt(document.getElementById("myForm").elements[3].value);
+	formData.decription = document.getElementById("myForm").elements[4].value;
+	post.order =JSON.stringify(formData);
 	post.idTour = idTour;
 	$.ajax({
 	  type: "POST",
